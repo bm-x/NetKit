ba70997a8406e371ade366ce404f800d33a0f657
clyde <236678253@qq.com>
2017-11-24 18:28:30 +0800

    add

--- netkit/src/main/java/com/okfunc/netkit/cache/StreamDiffPolicy.kt
+++ netkit/src/main/java/com/okfunc/netkit/cache/StreamDiffPolicy.kt
@@ -1,5 +1,6 @@
 package com.okfunc.netkit.cache
 
+import com.okfunc.netkit.NetKit
 import com.okfunc.netkit.NkCall
 import okhttp3.Call
 import okhttp3.Response
@@ -11,13 +12,24 @@ import java.io.File
 
 class StreamDiffPolicy : ICachePolicy {
 
+    var directory: File? = null
 
+    init {
+        NetKit.globalConfig.application?.let {
+            directory = File(it.cacheDir, this::class.simpleName)
+            directory?.mkdirs()
+        }
+    }
 
     override fun onStart(call: NkCall) {
+        val dir = directory ?: return
+
+        
 
     }
 
     override fun onResponse(call: Call, response: Response, block: (call: Call, response: Response) -> Unit) {
+        val dir = directory ?: return block(call, response)
 
     }
 }
